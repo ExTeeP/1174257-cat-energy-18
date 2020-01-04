8b8a67e0b351401d9605fdfc19658a03041c4441
ExTeeP <exteep@yandex.ru>
2020-01-04 15:36:01 +0700

    добавил скрипт меню

---
+++ source/js/script.js
@@ -0,0 +1,19 @@
+var navMain = document.querySelector('.main-nav');
+var headerToggle = document.querySelector('.header__toggle');
+
+headerToggle.classList.remove('header__toggle--nojs');
+navMain.classList.remove('main-nav--nojs');
+
+headerToggle.addEventListener('click', function() {
+	if (navMain.classList.contains('main-nav--closed')) {
+		navMain.classList.remove('main-nav--closed');
+		navMain.classList.add('main-nav--opened');
+		headerToggle.classList.add('header__toggle--close');
+		headerToggle.classList.remove('header__toggle--open');
+	} else {
+		navMain.classList.add('main-nav--closed');
+		navMain.classList.remove('main-nav--opened');
+		headerToggle.classList.remove('header__toggle--close');
+		headerToggle.classList.add('header__toggle--open');
+	}
+});
