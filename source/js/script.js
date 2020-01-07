d915c8a81caf41cb4751cdd32caffa079316d51d
ExTeeP <exteep@yandex.ru>
2020-01-07 14:10:42 +0700

    обьединил медиа запросы и сиправил ошибки

--- source/js/script.js
+++ source/js/script.js
@@ -5,15 +5,15 @@ headerToggle.classList.remove('header__toggle--nojs');
 navMain.classList.remove('main-nav--nojs');
 
 headerToggle.addEventListener('click', function() {
-	if (navMain.classList.contains('main-nav--closed')) {
-		navMain.classList.remove('main-nav--closed');
-		navMain.classList.add('main-nav--opened');
-		headerToggle.classList.add('header__toggle--close');
-		headerToggle.classList.remove('header__toggle--open');
-	} else {
-		navMain.classList.add('main-nav--closed');
-		navMain.classList.remove('main-nav--opened');
-		headerToggle.classList.remove('header__toggle--close');
-		headerToggle.classList.add('header__toggle--open');
-	}
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+    headerToggle.classList.add('header__toggle--close');
+    headerToggle.classList.remove('header__toggle--open');
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+    headerToggle.classList.remove('header__toggle--close');
+    headerToggle.classList.add('header__toggle--open');
+  }
 });
